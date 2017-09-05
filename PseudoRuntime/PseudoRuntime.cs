24a634fcf75b4228d437b90b1a92b32c34d2e891
Akira Kido <achts.y@gmail.com>
2017-09-05 18:46:57 +0900

    アセンブラを実行できるランタイム追加

---
+++ PseudoRuntime/PseudoRuntime.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace Runtime
+{
+    public static class PseudoRuntime
+    {
+        private static readonly float[] Registers = new float[10];
+
+        public static void Run( string s )
+        {
+            using ( var reader = new StringReader( s ) )
+            {
+                string line;
+                while ( (line = reader.ReadLine()) != null )
+                {
+                    var components = line.Split( new []{' '}, StringSplitOptions.RemoveEmptyEntries );
+                    switch ( components.Length )
+                    {
+                        case 3:
+                            var register = GetRegisterNum( components[0] );
+                            Registers[register] = float.Parse( components[2] );
+                            continue;
+                        case 5:
+                            var inputRegister = GetRegisterNum( components[0] );
+                            var register1 = GetRegisterNum( components[2] );
+                            var register2 = GetRegisterNum( components[4] );
+                            switch ( components[3] )
+                            {
+                                case "+":
+                                    Registers[inputRegister] = Registers[register1] + Registers[register2];
+                                    break;
+                                case "*":
+                                    Registers[inputRegister] = Registers[register1] * Registers[register2];
+                                    break;
+                                default:
+                                    throw new NotImplementedException();
+                            }
+                            continue;
+                    }
+                    throw new NotImplementedException();
+                }
+            }
+            Console.WriteLine(Registers[0]);
+        }
+
+        private static int GetRegisterNum(string s)
+        {
+            return (int) char.GetNumericValue( s[1] );
+        }
+    }
+}
--- SimpleCompiler/Program.cs
+++ SimpleCompiler/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Runtime;
 
 namespace SimpleCompiler
 {
@@ -6,9 +7,14 @@ namespace SimpleCompiler
     {
         static void Main(string[] args)
         {
-            var lexer = new SimpleLexer("1 + 2 * 3 + 4;");
-            var parser = new Parser( lexer, new ConsoleOutput() );
+            var lexer = new SimpleLexer("2 * 3 * 4;");
+            //var parser = new Parser( lexer, new ConsoleOutput() );
+
+            var output = new RuntimeOutput();
+            var parser = new Parser( lexer, output );
             parser.Statement();
+
+            PseudoRuntime.Run( output.Result );
         }
 
         private class ConsoleOutput : ICodeOutputDelegate
@@ -16,6 +22,16 @@ namespace SimpleCompiler
             public void Output( string code )
             {
                 Console.WriteLine(code);
+            }
+        }
+
+        private class RuntimeOutput : ICodeOutputDelegate
+        {
+            internal string Result { get; private set; } = string.Empty;
+
+            public void Output( string code )
+            {
+                Result += code + Environment.NewLine;
             }
         }
     }
